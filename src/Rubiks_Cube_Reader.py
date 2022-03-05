9d46f0a4a82293b77df54e4fc9a65c44c7455d62
Doron Feldman <dfeldman49@gmail.com>
2022-03-04 22:22:50 -0500

    - Added bounding box jpg to be superimposed on the screen for placement of the cube to be read. - Saves image whenever space bar is pressed.

--- src/Rubiks_Cube_Reader.py
+++ src/Rubiks_Cube_Reader.py
@@ -11,16 +11,18 @@ class CubeReader:
         if not cap.isOpened():
             raise IOError("Cannot open webcam")
 
+        bound = cv2.imread('Bounding_Box.jpg')
+            
         while True:
             ret, frame = cap.read()
             frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-            cv2.imshow('Input', frame)
+            cv2.imshow('Input', cv2.addWeighted(frame, 1, bound, 0.2, 0))
 
             c = cv2.waitKey(1)
-            if c == 27:
+            if c == 32: # if space bar was pressed
+                cv2.imwrite('test_image.jpg', frame)
+            if c == 27: # if esc key was pressed
                 break
-
-            cv2.imwrite('test_image.jpg', frame)
 
         cap.release()
         cv2.destroyAllWindows()
